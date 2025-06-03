09ad2dafac5c4356397b920e76a67e31d5f82e5e
Rikrdo05 <125338067+Rikrdo05@users.noreply.github.com>
2025-06-03 17:41:04 +0800

    Update app.py

--- app.py
+++ app.py
@@ -751,14 +751,14 @@ if st.session_state.calculate:
     metrics = [
         {"Metric": "Average Monthly Renewal Rate (%)", "Type": "constant", "Value": f"{renewal_rate:.1%}"},
         {"Metric": "User Subscription Life Time Value - LTV ($)", "Type": "constant", "Value": f"${LTV:,.2f}"},
-        {"Metric": "SEM Paid Customer Acquisition Cost - CAC ($)", "Type": "january", "Col": "sem_cpa"},
-        {"Metric": "SEM Paid Customer Acquisition Cost ROI ($)", "Type": "january", "Col": "sem_roi"},
-        {"Metric": "SEM Paid Customer Acquisition Cost ROI (%)", "Type": "january", "Col": "sem_roi_percent"},
-        {"Metric": "Affiliate Marketing Customer Acquisition Cost - CAC ($)", "Type": "constant", "Value": f"${affiliate_cpa:,.2f}"},
-        {"Metric": "Affiliate Marketing Customer Acquisition Cost ROI ($)", "Type": "constant", "Value": f"${affiliate_marketing_roi:,.2f}" if affiliate_marketing_roi_percent is not None else "N/A"},
-        {"Metric": "Affiliate Marketing Customer Acquisition Cost ROI (%)", "Type": "constant", "Value": f"{affiliate_marketing_roi_percent:.2%}" if affiliate_marketing_roi_percent is not None else "N/A"},
-        {"Metric": "Time to Recover SEM CAC (months)", "Type": "january", "Col": "time_to_recover_SEM_cac"},
-        {"Metric": "Time to Recover Affiliate CAC (months)", "Type": "january", "Col": "time_to_recover_AM_cac"}
+        {"Metric": "SEM Paid Customer Acquisition<br>Cost - CAC ($)", "Type": "january", "Col": "sem_cpa"},
+        {"Metric": "SEM Paid Customer Acquisition<br>Cost ROI ($)", "Type": "january", "Col": "sem_roi"},
+        {"Metric": "SEM Paid Customer Acquisition<br>Cost ROI (%)", "Type": "january", "Col": "sem_roi_percent"},
+        {"Metric": "Affiliate Marketing Customer<br>Acquisition Cost - CAC ($)", "Type": "constant", "Value": f"${affiliate_cpa:,.2f}"},
+        {"Metric": "Affiliate Marketing Customer<br>Acquisition Cost ROI ($)", "Type": "constant", "Value": f"${affiliate_marketing_roi:,.2f}" if affiliate_marketing_roi_percent is not None else "N/A"},
+        {"Metric": "Affiliate Marketing Customer<br>Acquisition Cost ROI (%)", "Type": "constant", "Value": f"{affiliate_marketing_roi_percent:.2%}" if affiliate_marketing_roi_percent is not None else "N/A"},
+        {"Metric": "Time to Recover<br>SEM CAC (months)", "Type": "january", "Col": "time_to_recover_SEM_cac"},
+        {"Metric": "Time to Recover<br>Affiliate CAC (months)", "Type": "january", "Col": "time_to_recover_AM_cac"}
     ]
 
     # Create the metrics dataframe
