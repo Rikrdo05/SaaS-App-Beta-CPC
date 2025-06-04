b53a40d63b055549b28cbba0658e729572ba25df
Rikrdo05 <125338067+Rikrdo05@users.noreply.github.com>
2025-06-04 14:29:06 +0800

    Update app.py

--- app.py
+++ app.py
@@ -178,11 +178,11 @@ with st.form("single_page_form", clear_on_submit=False):
     # Other Revenue Sources
     st.markdown("")
     st.subheader("Other Revenue - Ad Network (e.g, Google AdSense)")
-    st.write("Set the below values to 0.00 if Ad Network is not part of your revenue model")
+    st.write("Set Revenue Per Mile/1,000 Impressions - RPM ($) to 0.00 if Ad Network is not part of your revenue model")
     AdNet_col1, AdNet_col2 = st.columns(2)
     
     with AdNet_col1:
-        st.session_state.form_data['views per visit'] = st.number_input("Page Views Per Visit", min_value=1.0, value=0.0, step=0.5, format="%.2f")
+        st.session_state.form_data['views per visit'] = st.number_input("Page Views Per Visit", min_value=1.0, value=1.0, step=0.5, format="%.2f")
         
     with AdNet_col2:
         st.session_state.form_data['cpm'] = st.number_input("Revenue Per Mile/1,000 Impressions - RPM ($)", min_value=0.0, value=0.0, step=0.5, format="%.2f")
